4d5d54ea2ba1857cfda833455c753d9be4fc2a72
scirrincione <sccirrincione@gmail.com>
2025-02-24 12:14:37 -0600

    added contact info

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,9 +8,12 @@ function App() {
 
   return (
     <div className="bg-bgColor min-h-screen flex flex-col">
-      <header className="p-4 text-left bg-bgColor2 rounded">
+      <header className="p-4 text-left bg-bgColor2 rounded justify-between">
         <h1 className="text-3xl text-textColor">Sofia Cirrincione</h1>
+        <h1 className="text-l text-textColor text-right">847-284-8385</h1>
+        <h1 className="text-l text-textColor text-right">sccirrincione@gmail.com</h1>
       </header>
+
     </div>
   )
 }
