cb63a46641eaf32620d63ce9e6f19ea2d94b2f70
Sofia Cirrincione <cirri006@umn.edu>
2025-03-01 22:09:52 -0600

    updated url

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router'
 import RouterConfig from './RouterConfig.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/portfolio_website">
     <StrictMode>
       <RouterConfig />
     </StrictMode>,
